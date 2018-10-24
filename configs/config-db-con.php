a033e04d403190b50455054cb4e43ac5babe8bd5
Erin Miller <erinemillerr@gmail.com>
2018-10-24 18:33:27 -0400

    second

---
+++ configs/config-db-con.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = "localhost"; // add SQL server host
+    $user = "root"; //SQL username
+    $pass = "root"; //SQL password
+    $dbname = "guestbook"; //SQL Database Name
+
+    $con = mysqli_connect($host, $user, $pass, $dbname);
+
+?>
--- configs/configs-post.php
+++ configs/configs-post.php
@@ -1,7 +1,13 @@
 <?php
 /**
  * Set up POST variables to connect DB
+ * * Sets up DB connection for our from using POST vars
  */
+
+// var_dump($name); //*Dump information about a variable to the view.
+// print_r($name); //Essentially the same as dump. It prints infor about a variable, boolean, string, etc.
+// die; //*This version of die simply kills the program at the point in the code where you declare it.
+// die($name); //This version of die; allows you to kill the program AND print information about the variable.
 ?>
 
 
@@ -17,11 +23,19 @@
 
 
     $name = $_POST['name'];
+    $lastName=$_POST['lastName'];
+    $phone=$_POST['phone'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $sql = "INSERT INTO entries(name, email, message) VALUES('$name', '$email','$message')";
-
+    $sql = "INSERT INTO entries(name, lastName, phone, email, message) VALUES('$name',  '$lastName', '$phone','$email','$message')";
+    if(!mysqli_query($con,$sql)){  //If we are not connected to the DB ',' or our fields aren't recognized by our db
+        die('Error: ' . mysqli_error($con)); 
+    } else {
+        mysqli_close($con);
+    }
+    
+    ?>
 ?>
 
 <!-- 
